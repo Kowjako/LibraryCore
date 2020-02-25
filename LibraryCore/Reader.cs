f7469d39b559b486f32e165f9a933116af67503d
Włodzimierz Kowjako <kowyako@yandex.ru>
2020-02-25 18:29:53 +0100

    Add files via upload

---
+++ LibraryCore/Reader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCore
+{
+    public class Reader : People
+    {
+        public int sum { get; set; }
+        public Reader(string s, int a)
+        {
+            Name = s;
+            sum = a;
+        }
+    }
+}
---
+++ LibraryCore/Worker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCore
+{
+    class Worker : People
+    {
+        public Worker(string n)
+        {
+            Name = n;
+        }
+
+        public string DisplayInfo()
+        {
+            return $"Witam, Do pana dyspozycji : {Name}";
+        }
+    }
+}
